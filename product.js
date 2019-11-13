f08381c3ac967bab99496e07ff6128fef5425c1a
sukhada bhaip <sukhadabhaip@gmail.com>
2019-11-14 01:55:01 +0530

    6th commit

--- product.js
+++ product.js
@@ -1,5 +1,6 @@
-
+console.log("pppppp");
 console.log("welcome");
+console.log("welcome123...");
 const express = require('express')
 const mysql = require('mysql')
 
@@ -17,6 +18,19 @@ function connect() {
 }
 
 const app = express()
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 
